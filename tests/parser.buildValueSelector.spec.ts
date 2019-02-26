054a1a22c4ee912f639350809e1416feb6d3c79e
Denis Zavershinskiy <zaverden@gmail.com>
2019-02-26 23:00:55 +0700

    tests with Set and Map

--- tests/parser.buildValueSelector.spec.ts
+++ tests/parser.buildValueSelector.spec.ts
@@ -126,4 +126,39 @@ describe('buildValueSelector', () => {
     const value = selector(obj)
     expect(value).to.be.equal(`${date}`)
   })
+  it.skip('should handle Set as array (end)', () => {
+    // TODO: AssertionError: expected '[object Set]' to equal 'value1~value2'
+    const selector = buildValueSelector('a.b')
+    const obj = {
+      a: {
+        b: new Set(['value1', 'value2']),
+      },
+    }
+    const value = selector(obj)
+    expect(value).to.be.equal(`value1${ARRAY_ITEMS_DELIMITER}value2`)
+  })
+  it.skip('should handle Set as array (middle)', () => {
+    // TODO: AssertionError: expected '' to equal 'value'
+    const selector = buildValueSelector('a.b.c')
+    const obj = {
+      a: {
+        b: new Set([{ c: 'value' }]),
+      },
+    }
+    const value = selector(obj)
+    expect(value).to.be.equal('value')
+  })
+  it.skip('should handle Map as object', () => {
+    // TODO: AssertionError: expected '' to equal 'value'
+    const selector = buildValueSelector('a.b.c.d')
+    const obj = {
+      a: {
+        b: new Map([
+          ['c', { d: 'value' }],
+        ]),
+      },
+    }
+    const value = selector(obj)
+    expect(value).to.be.equal('value')
+  })
 })
