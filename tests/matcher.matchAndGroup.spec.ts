09894c5baf6c2f92054ec8b7eac201b635987b22
Denis Zavershinskiy <zaverden@gmail.com>
2019-04-09 20:47:14 +0700

    add NOT test for same field

--- tests/matcher.matchAndGroup.spec.ts
+++ tests/matcher.matchAndGroup.spec.ts
@@ -7,18 +7,25 @@ const csOptions: ParseOptions = { caseSensitiveFields: true }
 
 describe('matchAndGroup', () => {
   it('should match if all fields are matched', () => {
-    const field = parseAndGroup('a:there AND b:too', csOptions)
-    const matched = matchAndGroup(field, { a: 'hello there', b: 'hello there too' })
+    const andGroup = parseAndGroup('a:there AND b:too', csOptions)
+    const matched = matchAndGroup(andGroup, { a: 'hello there', b: 'hello there too' })
     expect(matched).to.true
   })
   it('should not match if some field is not matched', () => {
-    const field = parseAndGroup('a:there AND b:too', csOptions)
-    const matched = matchAndGroup(field, { a: 'hello there', b: 'hello there again' })
+    const andGroup = parseAndGroup('a:there AND b:too', csOptions)
+    const matched = matchAndGroup(andGroup, { a: 'hello there', b: 'hello there again' })
     expect(matched).to.false
   })
   it('should match with NOT respect', () => {
-    const field = parseAndGroup('a:there AND NOT b:too', csOptions)
-    const matched = matchAndGroup(field, { a: 'hello there', b: 'hello there again' })
+    const andGroup = parseAndGroup('a:there AND NOT b:too', csOptions)
+    const matched = matchAndGroup(andGroup, { a: 'hello there', b: 'hello there again' })
     expect(matched).to.true
+  })
+  it('should match same field with NOT respect', () => {
+    const andGroup = parseAndGroup('status: error AND NOT status: unknown', csOptions)
+    const matched1 = matchAndGroup(andGroup, { status: 'error/timeout' })
+    const matched2 = matchAndGroup(andGroup, { status: 'error/unknown' })
+    expect(matched1).to.true
+    expect(matched2).to.false
   })
 })
